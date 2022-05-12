d015031465c2f7ebbc8e7f9994c8d9069e07d64d
PrzeChoj <premysl.choj@gmail.com>
2022-05-13 01:44:05 +0200

    alg small develomplent

--- R/algorithm.R
+++ R/algorithm.R
@@ -1,20 +1,62 @@
+source("utils.R")
+
 #' W mutacji liczba transpozycji będzie losowana z rozkladu binomialnego
-evolutional_optimization <- function(U, n_number, max_iter, pop_size=100,
-                                     mutation_factor=0.8, tournament_size=2,
-                                     perm_size=NULL, delta=3, D_matrix=NULL,
-                                     use_progress_bar=TRUE){
-  if(is.null(perm_size)){
-    perm_size <- dim(U)[1]
-  }
+evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=100,
+                                     success_treshold=0.2, a=0.817,
+                                     tournament_size=2,
+                                     delta=3, D_matrix=NULL,
+                                     use_progress_bar=TRUE){ # TODO(use_progress_bar)
+  perm_size <- dim(U)[1]
+  
   stopifnot(perm_size == dim(U)[1])
   if(is.null(D_matrix)){
     D_matrix <- diag(nrow = perm_size)
   }
   
+  f_values <- numeric(pop_size)
+  best_f_value <- 0  # f is always positive, so this is smaller than any f value
+  best_permutation <- NULL
+  
   population <- list()
   for(i in 1:pop_size){
     population[[i]] <-  runif_perm(perm_size)
+    f_values[i] <- goal_function(population[[i]],
+                                 perm_size, n_number, U,
+                                 delta=delta, D_matrix=D_matrix)
+    
+    if(f_values[i] > best_f_value){
+      best_f_value <- f_values[i]
+      best_permutation <- population[[i]]
+    }
   }
   
-  # TODO
+  for(iteration in 2:max_iter){
+    # TODO
+  }
+  
+  list("best_f_value" = best_f_value, "best_permutation" = best_permutation)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
