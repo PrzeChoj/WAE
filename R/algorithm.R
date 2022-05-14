4d3775e29b657916cd5449692371c1d6869d611d
PrzeChoj <premysl.choj@gmail.com>
2022-05-14 20:32:16 +0200

    best_value saving

--- R/algorithm.R
+++ R/algorithm.R
@@ -4,8 +4,11 @@ source("R/utils.R")
 #' 
 #' @examples
 #' n_number <- 20
-#' U <- U_maker(p=10, n=n_number)
-#' evolutional_optimization(U, n_number, max_iter=10, pop_size=15)
+#' U <- U_maker(p=5, n=n_number)
+#' out <- evolutional_optimization(U, n_number, max_iter=100, pop_size=3)
+#' out[["best_f_value"]]
+#' out[["best_permutation"]]
+#' out <- evolutional_optimization(U, n_number, max_iter=100, pop_size=3, a=1)
 evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=100,
                                      success_treshold=0.2, a=0.817,
                                      tournament_size=2,
@@ -27,7 +30,7 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=100,
   }
   
   f_values <- numeric(pop_size)
-  best_f_value <- 0  # f is always positive, so this is smaller than any f value
+  best_f_value_list <- 0  # f is always positive, so this is smaller than any f value
   best_permutation <- NULL
   
   # init population
@@ -36,8 +39,10 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=100,
     population[[i]] <-  runif_perm(perm_size)
     f_values[i] <- my_goal_function(population[[i]])
   }
-  best_f_value <- max(f_values)
-  best_permutation <- population[[which(f_values == best_f_value)[1]]]
+  best_f_value_list <- max(f_values)
+  names(best_f_value_list) <- "1"
+  best_f_value <- best_f_value_list[1]
+  best_permutation <- population[[which(f_values == best_f_value_list)[1]]]
   
   # mail loop
   for(iteration in 2:max_iter){
@@ -51,16 +56,24 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=100,
       f_values[i] <- my_goal_function(population[[i]])
     }
     best_f_value_new <- max(f_values)
-    if(best_f_value_new > best_f_value){
+    num_of_best_values <- length(best_f_value_list)
+    
+    # save the best
+    if(best_f_value_new > best_f_value_list[num_of_best_values]){
+      best_f_value_list[num_of_best_values+1] <- best_f_value_new
+      names(best_f_value_list) <- c(names(best_f_value_list)[1:num_of_best_values],
+                               as.character(iteration))
       best_f_value <- best_f_value_new
-      best_permutation <- population[[which(f_values == best_f_value)[1]]]
+      best_permutation <- population[[which(f_values == best_f_value_new)[1]]]
     }
   }
   
   if(show_progress_bar)
     close(progressBar)
   
-  list("best_f_value" = best_f_value, "best_permutation" = best_permutation)
+  list("best_f_value_list" = best_f_value_list,
+       "best_permutation" = best_permutation,
+       "best_f_value" = best_f_value)
 }
 
 
