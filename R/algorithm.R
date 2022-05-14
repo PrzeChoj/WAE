133f698ae0bc345be031638fef8c6848ec040627
PrzeChoj <premysl.choj@gmail.com>
2022-05-14 15:40:41 +0200

    progress_bar; maker functions

--- R/algorithm.R
+++ R/algorithm.R
@@ -1,38 +1,64 @@
-source("utils.R")
+source("R/utils.R")
 
 #' W mutacji liczba transpozycji będzie losowana z rozkladu binomialnego
+#' 
+#' @examples
+#' n_number <- 20
+#' U <- U_maker(p=10, n=n_number)
+#' evolutional_optimization(U, n_number, max_iter=10, pop_size=15)
 evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=100,
                                      success_treshold=0.2, a=0.817,
                                      tournament_size=2,
                                      delta=3, D_matrix=NULL,
-                                     use_progress_bar=TRUE){ # TODO(use_progress_bar)
+                                     show_progress_bar=TRUE){
+  if(show_progress_bar)
+    progressBar = utils::txtProgressBar(min = 0, max = max_iter, initial = 1)
+  
+  stopifnot(dim(U)[1] == dim(U)[2])
   perm_size <- dim(U)[1]
   
-  stopifnot(perm_size == dim(U)[1])
   if(is.null(D_matrix)){
     D_matrix <- diag(nrow = perm_size)
+  }
+  
+  my_goal_function <- function(perm){
+    gips::goal_function(perm, perm_size, n_number, U,
+                        delta=delta, D_matrix=D_matrix)
   }
   
   f_values <- numeric(pop_size)
   best_f_value <- 0  # f is always positive, so this is smaller than any f value
   best_permutation <- NULL
   
+  # init population
   population <- list()
   for(i in 1:pop_size){
     population[[i]] <-  runif_perm(perm_size)
-    f_values[i] <- goal_function(population[[i]],
-                                 perm_size, n_number, U,
-                                 delta=delta, D_matrix=D_matrix)
+    f_values[i] <- my_goal_function(population[[i]])
+  }
+  best_f_value <- max(f_values)
+  best_permutation <- population[[which(f_values == best_f_value)[1]]]
+  
+  # mail loop
+  for(iteration in 2:max_iter){
+    if(show_progress_bar)
+      utils::setTxtProgressBar(progressBar, iteration)
     
-    if(f_values[i] > best_f_value){
-      best_f_value <- f_values[i]
-      best_permutation <- population[[i]]
+    # TODO(Teraz dałem szukanie monte carlo)
+    population <- list()
+    for(i in 1:pop_size){
+      population[[i]] <-  runif_perm(perm_size)
+      f_values[i] <- my_goal_function(population[[i]])
+    }
+    best_f_value_new <- max(f_values)
+    if(best_f_value_new > best_f_value){
+      best_f_value <- best_f_value_new
+      best_permutation <- population[[which(f_values == best_f_value)[1]]]
     }
   }
   
-  for(iteration in 2:max_iter){
-    # TODO
-  }
+  if(show_progress_bar)
+    close(progressBar)
   
   list("best_f_value" = best_f_value, "best_permutation" = best_permutation)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,32 @@
-goal_function_maker <- function(p, n, sigma=NULL){
+library(permutations)
+library(gips)  # devtools::install_github("PrzeChoj/gips")
+
+sigma_maker <- function(p){
+  sigma <- matrix(numeric(p*p), nrow=p)
+  for(i in 1:p){
+    for(j in 1:p){
+      sigma[i,j] <- 1 - min(abs(i-j), p-abs(i-j)) / p
+    }
+    sigma[i,i] <- 1 + 1/p
+  }
+  
+  sigma
+}
+
+U_maker <- function(p, n, sigma=NULL){
   mu <- numeric(p)
   if(is.null(sigma)){
-    sigma <- matrix(numeric(p*p), nrow=p)
-    for(i in 1:p){
-      for(j in 1:p){
-        sigma[i,j] <- 1 - min(abs(i-j), p-abs(i-j)) / p
-      }
-      sigma[i,i] <- 1 + 1/p
-    }
+    sigma <- sigma_maker(p)
   }
   
   Z <- MASS::mvrnorm(n, mu = mu, Sigma = sigma)
   U <- t(Z) %*% Z
+  
+  U
+}
+
+goal_function_maker <- function(p, n, sigma=NULL){
+  U <- U_maker(p, n, sigma)
   
   my_goal_function <- function(perm){
     goal_function(perm, p, n, U)
@@ -23,13 +38,16 @@ goal_function_maker <- function(p, n, sigma=NULL){
 }
 
 
-#' Draw a random permutation; Drawing a random transosition is in gips:::runif_transposition
+#' Draw a random permutation
 runif_perm <- function(perm_size){
   permutations::as.cycle(permutations::as.word(sample(perm_size, perm_size,
                                                       replace=FALSE)))
 }
 
-
+#' Drawing a random transosition
+runif_transposition <- function(perm_size){
+  permutations::as.cycle(sample(perm_size, perm_size, replace=FALSE))
+}
 
 
 
