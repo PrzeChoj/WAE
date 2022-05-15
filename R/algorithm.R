47217f60fa26a42924e75c56e138e0718523a3e6
PrzeChoj <premysl.choj@gmail.com>
2022-05-15 23:19:24 +0200

    succession; more info in output

--- R/algorithm.R
+++ R/algorithm.R
@@ -19,18 +19,26 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=3,
     progressBar <- utils::txtProgressBar(min = 0, max = max_iter, initial = 1)
   
   stopifnot(dim(U)[1] == dim(U)[2])
+  stopifnot(pop_size >= tournament_size)
   perm_size <- dim(U)[1]
   
   if(is.null(D_matrix)){
     D_matrix <- diag(nrow = perm_size)
   }
   
-  p_t <- p_0
+  p_t_list <- numeric(max_iter)
+  p_t_list[TRUE] <- NA
+  p_t_list[1] <- p_0
+  success_rate_list <- numeric(max_iter)
+  success_rate_list[TRUE] <- NA
   
   my_goal_function <- function(perm){
     gips::goal_function(perm, perm_size, n_number, U,
                         delta=delta, D_matrix=D_matrix)
   }
+  
+  mean_f_value_list <- numeric(max_iter)
+  mean_f_value_list[TRUE] <- NA
   
   f_values <- numeric(pop_size)
   best_f_value_list <- 0  # f is always positive, so this is smaller than any f value
@@ -39,20 +47,19 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=3,
   # init population
   population <- list()
   for(i in 1:pop_size){
-    population[[i]] <-  runif_perm(perm_size)
+    population[[i]] <- runif_perm(perm_size)
     f_values[i] <- my_goal_function(population[[i]])
   }
   best_f_value_list <- max(f_values)
   names(best_f_value_list) <- "1"
   best_f_value <- best_f_value_list[1]
   best_permutation <- population[[which(f_values == best_f_value_list)[1]]]
+  mean_f_value_list[1] <- mean(f_values)
   
   # mail loop
   for(iteration in 2:max_iter){
     if(show_progress_bar)
       utils::setTxtProgressBar(progressBar, iteration)
-    
-    # TODO(iteracja)
     
     # TODO(Reprodukcja)
     reproduced <- population
@@ -64,7 +71,7 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=3,
     mutants_f_values <- numeric(pop_size)
     
       # draw number of mutations for a specimens
-    num_of_mutations <- pmax(rbinom(pop_size, k_max, p_t), 1)
+    num_of_mutations <- pmax(rbinom(pop_size, k_max, p_t_list[iteration-1]), 1)
     for(i in 1:pop_size){
       mutation_perm <- permutations::id
       for(j in 1:num_of_mutations[i]){
@@ -75,18 +82,32 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=3,
       mutants_f_values[i] <- my_goal_function(mutants[[i]])
     }
     
-    success_rate <- mean(mutants_f_values > reproduced_f_value)
+      # modify p_t
+    success_rate_list[iteration] <- mean(mutants_f_values > reproduced_f_value)
     
-    if(success_rate > success_treshold){
-      p_t <- 1 - (1 - p_t) * a # p_t will be bigger
+    if(success_rate_list[iteration] > success_treshold){
+      p_t_list[iteration] <- 1 - (1 - p_t_list[iteration-1]) * a # p_t will be bigger
     }else{
-      p_t <- p_t * a # p_t will be smaller
+      p_t_list[iteration] <- p_t_list[iteration-1] * a # p_t will be smaller
     }
     
-    # TODO succession
-    population <- mutants
-    f_values <- mutants_f_values
-    # end of TODO
+    
+    # succession
+    population <- list()
+    for(i in 1:pop_size){
+      players <- sample(pop_size, tournament_size, replace = TRUE)
+      
+      tournament_f_values <- mutants_f_values[players]
+      winner <- which(tournament_f_values == max(tournament_f_values))[1]
+      
+      # TODO(Think or ask if all specimens will be mutated)
+      population[[i]] <- mutants[[winner]]
+      f_values[i] <- mutants_f_values[i]
+    }
+    
+    
+    # save the mean
+    mean_f_value_list[iteration] <- mean(f_values)
     
     # save the best
     best_f_value_mutant <- max(mutants_f_values)
@@ -106,7 +127,10 @@ evolutional_optimization <- function(U, n_number, max_iter=100, pop_size=3,
   
   list("best_f_value_list" = best_f_value_list,
        "best_permutation" = best_permutation,
-       "best_f_value" = best_f_value)
+       "best_f_value" = best_f_value,
+       "success_rate_list" = success_rate_list,
+       "p_t_list" = p_t_list,
+       "mean_f_value_list" = mean_f_value_list)
 }
 
 
